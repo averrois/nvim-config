41bcc2173b4deeeb60e7ad5421615cdc7289c67f
Yassin-Aru <jocarhisouka@gmail.com>
2024-04-04 17:49:22 +0000

    chore: setup treesiter

--- init.lua
+++ init.lua
@@ -27,7 +27,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
