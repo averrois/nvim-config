c4d653277afb221e34da2ede5c0679109ef01bbe
averrois <yassine.aitrahou0@gmail.com>
2024-10-04 16:26:07 +0100

    refactor: update format files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,46 +1,46 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "cssls",
-          "bashls",
-          "html",
-          "tsserver",
-          "tailwindcss",
-          "vuels",
-          "jsonls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.vuels.setup({})
-      lspconfig.jsonls.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"cssls",
+					"bashls",
+					"html",
+					"tsserver",
+					"tailwindcss",
+					"vuels",
+					"jsonls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.vuels.setup({})
+			lspconfig.jsonls.setup({})
 
-      -- Key Binding --
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			-- Key Binding --
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { "go", "c", "lua", "vim", "vimdoc", "vue", "css", "typescript", "bash", "javascript", "html" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+		configs.setup({
+			ensure_installed = {
+				"go",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"vue",
+				"css",
+				"typescript",
+				"bash",
+				"javascript",
+				"html",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
