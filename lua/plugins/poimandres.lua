d4a53fa56aa55ba13ae1e9e72150b2f213f611bd
averrois <yassine.aitrahou0@gmail.com>
2024-10-05 16:27:25 +0100

    fix: remove ~ from lines numbers

--- lua/plugins/poimandres.lua
+++ lua/plugins/poimandres.lua
@@ -4,14 +4,14 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-    require('poimandres').setup {
-      bold_vert_split = false,
-      dim_nc_background = false,
-      disable_background = false,
-      disable_float_background = false,
-      disable_italics = false,
-    }
-  end,
+		require("poimandres").setup({
+			bold_vert_split = false,
+			dim_nc_background = false,
+			disable_background = false,
+			disable_float_background = false,
+			disable_italics = false,
+		})
+	end,
 
 	-- optionally set the colorscheme within lazy config
 	init = function()
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.opt.number = true
+vim.opt.fillchars:append { eob = " " }
 
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
