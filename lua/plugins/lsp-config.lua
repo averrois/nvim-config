1fb696a72b778d77831ec22f82ef1dd6d8a9c2cf
averrois <yassine.aitrahou0@gmail.com>
2024-10-18 16:22:55 +0100

    feat: update the select ui: - install lsp saga to make the ui more nice

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,24 +24,58 @@ return {
 		end,
 	},
 	{
+		"nvimdev/lspsaga.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lspsaga").setup({
+				ui = {
+					border = "rounded",
+					title = true,
+				},
+				hover = {
+					max_width = 0.6,
+					max_height = 0.6,
+					open_link = "gx",
+					open_cmd = "!chrome",
+				},
+			})
+		end,
+	},
+	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.bashls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.tailwindcss.setup({ capabilities = capabilities })
-			lspconfig.vuels.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
 
-			-- Key Binding --
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- Setup all LSP servers
+			local servers = {
+				"lua_ls",
+				"gopls",
+				"cssls",
+				"bashls",
+				"html",
+				"ts_ls",
+				"tailwindcss",
+				"vuels",
+				"jsonls",
+			}
+
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+				})
+			end
+
+			-- Key Bindings using LSPSaga
+			vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+			vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+			vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+			vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>")
+			vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
+			vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
 		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,21 +5,68 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
 		config = function()
+			local actions = require("telescope.actions")
+
 			require("telescope").setup({
+				defaults = {
+					path_display = { "truncate" },
+					sorting_strategy = "ascending",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+						},
+						vertical = {
+							mirror = false,
+						},
+						width = 0.87,
+						height = 0.80,
+						preview_cutoff = 120,
+					},
+					mappings = {
+						i = {
+							["<C-n>"] = actions.cycle_history_next,
+							["<C-p>"] = actions.cycle_history_prev,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+					},
+				},
 				pickers = {
 					find_files = {
+						theme = "dropdown",
+						previewer = false,
 						hidden = true,
-						theme = "ivy",
+					},
+					buffers = {
+						theme = "dropdown",
+						previewer = false,
 					},
 				},
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+							initial_mode = "normal",
+							sorting_strategy = "ascending",
+							layout_strategy = "horizontal",
+							layout_config = {
+								horizontal = {
+									width = 0.5,
+									height = 0.4,
+									preview_width = 0.6,
+								},
+							},
+						}),
 					},
 				},
 			})
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
