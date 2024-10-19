a6e291bf8346408bf8b5695014b8dc9c7002a132
averrois <yassine.aitrahou0@gmail.com>
2024-10-19 13:59:28 +0100

    fix: resolve the glitch on numeric column

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,81 +1,81 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-					"cssls",
-					"bashls",
-					"html",
-					"ts_ls",
-					"tailwindcss",
-					"vuels",
-					"jsonls",
-				},
-			})
-		end,
-	},
-	{
-		"nvimdev/lspsaga.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("lspsaga").setup({
-				ui = {
-					border = "rounded",
-					title = true,
-				},
-				hover = {
-					max_width = 0.6,
-					max_height = 0.6,
-					open_link = "gx",
-					open_cmd = "!chrome",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "cssls",
+          "bashls",
+          "html",
+          "ts_ls",
+          "tailwindcss",
+          "vuels",
+          "jsonls",
+        },
+      })
+    end,
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lspsaga").setup({
+        ui = {
+          border = "rounded",
+          title = true,
+        },
+        hover = {
+          max_width = 0.6,
+          max_height = 0.6,
+          open_link = "gx",
+          open_cmd = "!chrome",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			-- Setup all LSP servers
-			local servers = {
-				"lua_ls",
-				"gopls",
-				"cssls",
-				"bashls",
-				"html",
-				"ts_ls",
-				"tailwindcss",
-				"vuels",
-				"jsonls",
-			}
+      -- Setup all LSP servers
+      local servers = {
+        "lua_ls",
+        "gopls",
+        "cssls",
+        "bashls",
+        "html",
+        "ts_ls",
+        "tailwindcss",
+        "vuels",
+        "jsonls",
+      }
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-				})
-			end
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+        })
+      end
 
-			-- Key Bindings using LSPSaga
-			vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
-			vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
-			vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-			vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>")
-			vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
-			vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
-		end,
-	},
+      -- Key Bindings using LSPSaga
+      vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+      vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+      vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+      vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>")
+      vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
+      vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.opt.number = true
-vim.opt.fillchars:append { eob = " " }
+vim.opt.fillchars:append({ eob = " " })
 
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
+
+-- Always show sign column to prevent text shifting
+vim.opt.signcolumn = "yes"  -- or "no" if you want to disable it completely
